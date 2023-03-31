557efe1a31e2ac4c241e82dc0b98421dbcbfc79e
Arnaldo Bismarques <arnaldo.bismarques@hotmail.com>
2023-03-31 20:03:45 +0200

    created an a_create.php file

---
+++ animals/actions/a_create.php
@@ -0,0 +1,70 @@
+<?php
+
+    require_once '../../components/db_connect.php';
+    require_once '../../components/file_upload.php';
+    
+    session_start();
+
+    if (!isset($_SESSION["user"]) && !isset($_SESSION["adm"])) {
+    header("Location: ../../index.php");
+    exit;
+    }
+    if(isset($_SESSION["user"])){
+    header("Location ../../home.php");
+}
+
+
+if ($_POST) {
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    $breed = $_POST['breed'];
+    $size = $_POST['size'];
+    $uploadError = '';
+    $picture = file_upload($_FILES['picture'], "product");
+
+    $sql = "INSERT INTO `animals`(name, age, breed, size, picture, ) VALUES 
+    ('$name', $age, '$breed', '$size', '$picture->fileName')";
+
+
+    if (mysqli_query($connect, $sql) === true) {
+        $class = "success";
+        $message = "The entry below was successfully created <br>
+            <table class='table w-50'><tr>
+            <td> $name </td>
+            <td> $price </td>
+            </tr></table><hr>";
+        $uploadError = ($picture->error != 0) ? $picture->ErrorMessage : '';
+    } else {
+        $class = "danger";
+        $message = "Error while creating file. Please try again! <br>" . $connect->error;
+        $uploadError = ($picture->error != 0) ? $picture->ErrorMessage : '';
+    }
+    mysqli_close($connect);
+} else {
+    header("location: ../error.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Update</title>
+    <?php require_once '../../components/boot.php' ?>
+</head>
+
+<body>
+    <div class="container">
+        <div class="mt-3 mb-3">
+            <h1>Create request response</h1>
+        </div>
+        <div class="alert alert-<?= $class; ?>" role="alert">
+            <p><?php echo ($message) ?? ''; ?></p>
+            <p><?php echo ($uploadError) ?? ''; ?></p>
+            <a href='../index.php'><button class="btn btn-primary" type='button'>Home</button></a>
+        </div>
+    </div>
+</body>
+</html>
