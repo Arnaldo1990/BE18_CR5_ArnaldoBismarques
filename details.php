7cbc72682c2a34d3554a5492f1f92dca3016190f
Arnaldo Bismarques <arnaldo.bismarques@hotmail.com>
2023-03-31 23:32:30 +0200

    finished

--- details.php
+++ details.php
@@ -1,35 +1,18 @@
 <?php
+require_once "components/db_connect.php";
+
 session_start();
 
-require_once 'components/db_connect.php';
-
-if (!isset($_SESSION['adm']) && !isset($_SESSION['user'])) {
-    header("Location: ../../details.php");
-    exit;
+if(!isset($_SESSION["user"])){
+    header("Location: index.php");
 }
 
-if (isset($_SESSION['user']) != "") {
-    header("Location: ../../home.php");
-    exit;
-}
+$id = $_GET["id"];
+$sql = "SELECT * FROM animals WHERE animalId = $id";
+$result = mysqli_query($connect, $sql);
 
-if ($_POST) {
-    $id = $_POST['id'];
-    $picture = $_POST['picture'];
-    ($picture == "animal.png") ?: unlink("../../pictures/$picture");
+$row = mysqli_fetch_assoc($result);
 
-    $sql = "DELETE FROM animals WHERE id = {$id}";
-    if (mysqli_query($connect, $sql) === TRUE) {
-        $class = "success";
-        $message = "Successfully Deleted!";
-    } else {
-        $class = "danger";
-        $message = "The entry was not deleted due to: <br>" . $connect->error;
-    }
-    mysqli_close($connect);
-} else {
-    header("location: ../error.php");
-}
 ?>
 
 <!DOCTYPE html>
@@ -37,19 +20,39 @@ if ($_POST) {
 
 <head>
     <meta charset="UTF-8">
-    <title>Delete</title>
-    <?php require_once '../../components/boot.php' ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <script type="text/javascript" src="scripts.js" defer></script>
+    <meta name="author" content="Arnaldo Bismarques">
+    <meta name="description" content="">
+    <title>Details</title>
 </head>
 
 <body>
-    <div class="container">
-        <div class="mt-3 mb-3">
-            <h1>Delete request response</h1>
-        </div>
-        <div class="alert alert-<?= $class; ?>" role="alert">
-            <p><?= $message; ?></p>
-            <a href='../index.php'><button class="btn btn-success" type='button'>Home</button></a>
-        </div>
-    </div>
+ <div>
+
+ <div class="card" style="width: 18rem;">
+  <img src="pictures/<?= $row['description'] ?>" class="card-img-top" alt="<?= $row["animal_name"] ?>">
+  <div class="card-body">
+    <h5 class="card-title"><?= $row["animal_name"] ?></h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">An item</li>
+    <li class="list-group-item">A second item</li>
+    <li class="list-group-item">A third item</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+    <p>ID: <?= $row['animalId']?></p>
+    <p><img src="pictures/<?= $row['description'] ?>"width="200"></p>
+    <p>Name: <?= $row["animal_name"] ?></p>
+    <p>Age: <?= $row["age"] ?></p>
+    <p>Vaccine: <?= $row["vaccine"] ?></p>
+</div>
 </body>
+
 </html>
