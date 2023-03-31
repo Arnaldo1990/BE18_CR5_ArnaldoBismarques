08a8a92e50dbb469db09218e754bc7cbbc3436be
Arnaldo Bismarques <arnaldo.bismarques@hotmail.com>
2023-03-31 16:20:54 +0200

    changes in the home.php file

--- home.php
+++ home.php
@@ -2,13 +2,15 @@
 
 session_start();
 
-if(!isset($_SESSION["user"])){
+if(isset($_SESSION["adm"])){
+    header("Location: dashboard.php");
+}elseif(!isset($_SESSION["user"])) {
     header("Location: index.php");
 }
 
-if(!isset($_SESSION["adm"])){
-    header("Location: dashboard.php");
-}
-
 require_once "components/db_connect.php";
+
+$sql = "SELECT * FROM users WHERE id = {$_SESSION["user"]}";
+$result = mysqli_query($connect, $sql);
+$row = mysqli_fetch_assoc($result);
 ?>
