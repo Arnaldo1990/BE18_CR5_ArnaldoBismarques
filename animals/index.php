5352479623729f86eb0a00e3d69104ec14841790
Arnaldo Bismarques <arnaldo.bismarques@hotmail.com>
2023-03-31 19:13:09 +0200

    changes

---
+++ animals/index.php
@@ -0,0 +1,75 @@
+<?php
+session_start();
+
+require_once '../components/db_connect.php';
+
+
+if (!isset($_SESSION["user"]) && !isset($_SESSION["adm"])) {
+    header("Location: ../index.php");
+    exit;
+}
+if(isset($_SESSION["user"])){
+    header("Location ../home.php");
+}
+
+$email = 'email';
+
+$sql = "SELECT id, first_name, password, status FROM users WHERE email = '$email'";
+$result = mysqli_query($connect, $sql);
+$tbody = ''; //this variable will hold the body for the table
+if (mysqli_num_rows($result)  > 0) {
+    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+        $tbody .= "<tr>
+            <td><img class='img-thumbnail' src='../pictures/" . $row['picture'] . "'</td>
+            <td>" . $row['name'] . "</td>
+            <td>" . $row['price'] . "</td>
+            <td>" . $row['sup_name'] . "</td>
+            <td><a href='update.php?id=" . $row['id'] . "'><button class='btn btn-primary btn-sm' type='button'>Edit</button></a>
+            <a href='delete.php?id=" . $row['id'] . "'><button class='btn btn-danger btn-sm' type='button'>Delete</button></a></td>
+            </tr>";
+    };
+} else {
+    $tbody =  "<tr><td colspan='6'><center>No Data Available </center></td></tr>";
+}
+
+mysqli_close($connect);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listed animals</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <?php require_once '../components/boot.php' ?>
+    <style type="text/css">
+        
+    </style>
+</head>
+
+<body>
+    <div class="manageProduct w-75 mt-3">
+        <div class='mb-3'>
+            <a href="create.php"><button class='btn btn-outline-primary' type="button">Add product</button></a>
+            <a href="../index.php"><button class='btn btn-outline-warning' type="button">Dashboard</button></a>
+        </div>
+        <p class='h2'>Animals</p>
+        <table class='table table-striped'>
+            <thead class='table-success'>
+                <tr>
+                    <th>Picture</th>
+                    <th>Name</th>
+                    <th>Age</th>
+                    <th>Size</th>
+                    <th>Breed</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?= $tbody; ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
