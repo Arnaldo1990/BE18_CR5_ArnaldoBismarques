2ccf4683ba74506bce27cb41b84d09ac047d065c
Arnaldo Bismarques <arnaldo.bismarques@hotmail.com>
2023-03-31 16:15:00 +0200

    changes in the dashboard.php file

--- home.php
+++ home.php
@@ -2,8 +2,13 @@
 
 session_start();
 
+if(!isset($_SESSION["user"])){
+    header("Location: index.php");
+}
 
-
+if(!isset($_SESSION["adm"])){
+    header("Location: dashboard.php");
+}
 
 require_once "components/db_connect.php";
 ?>
