57604b22622a39e75126cff7a2fd21e228cd4eb2
Arnaldo Bismarques <arnaldo.bismarques@hotmail.com>
2023-04-01 14:52:03 +0200

    finished

--- components/file_upload.php
+++ components/file_upload.php
@@ -3,7 +3,7 @@
 function file_upload($picture)
 {
     $result = new stdClass(); //this object will carry status from file upload
-    $result->fileName = 'avatar.png';
+    $result->fileName = 'avatar.jpg';
     $result->error = 1; //it could also be a boolean true/false
     //collect data from object $picture
     $fileName = $picture["name"];
